1a7fba8db260febd1cf1050cbc6e502147e49a29
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2019-05-13 00:11:50 +0300

    feat: implement change color function for red

--- move.js
+++ move.js
@@ -4,3 +4,18 @@ const redColor = '#e83740';
 const blueColor = '#4286f4';
 const orangeColor = '#ed9a42';
 const grayColor = '#a8a29c';
+window.state = {
+    currentTool: 'none'
+};
+//Declarations for shortcuts of tools
+const load = document.querySelector('#load');
+const save = document.querySelector('#save');
+const head = document.querySelector('#reset');
+//Colors
+//Red
+document.querySelector('#red-color').addEventListener('click', function() {
+    prevColor = currentColor;
+    $('#prev-container').css('background-color', prevColor);
+    currentColor = redColor;
+    $('#current-container').css('background-color', currentColor);
+});
